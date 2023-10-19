53819813b9f34d3b34d7a14107b357c1c0ceea7c
Success-akinyemi <brownsamuel915@gmail.com>
2023-10-18 19:46:31 -0700

    editing

--- client/src/Pages/EditHouse/EditHouse.jsx
+++ client/src/Pages/EditHouse/EditHouse.jsx
@@ -12,36 +12,66 @@ function EditHouse({isOpen, toggle}) {
     const { isLoadingHouseData, apiHouseData, houseServerError, houseStatus } = useFetchHouses(path)
     const { _id, address, desc, image, imageArray, location, price, title, } = apiHouseData?.data?.house || {}
 
+
+    const [formData, setFormData] = useState({
+      title: title || '',
+      desc: desc || '',
+      address: address || '',
+      location: location || '',
+      price: price || '',
+    });
+  
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    };
+  
+    const handleFormSubmit = (e) => {
+      e.preventDefault();
+      // Create an object containing only the fields that have changed
+      const updatedFields = {};
+      for (const key in formData) {
+        if (formData[key] !== (apiHouseData?.data?.house[key] || '')) {
+          updatedFields[key] = formData[key];
+        }
+      }
+      // Send updatedFields to your API for updating the house
+      console.log(updatedFields);
+    };
+
   return (
     <div className='editHouse'>
         <Navbar toggle={toggle} />
         <DropDown isOpen={isOpen} toggle={toggle} />
         <div className="editHouse-container">
             <h2>EditHouse</h2>
-            <form>
+            <form onSubmit={handleFormSubmit}>
               <div className="input-body">
                 <label htmlFor="">Tilte:</label>
-                <input type="text"  />
+                <input type="text" name='title' value={formData.title} onChange={handleInputChange} />
               </div>
 
               <div className="input-body">
                 <label htmlFor="">Description:</label>
-                <input type="text"  />
+                <input type="text" name='desc' value={formData.desc} onChange={handleInputChange} />
               </div>
 
               <div className="input-body">
                 <label htmlFor="">Address:</label>
-                <input type="text"  />
+                <input type="text" name='address' value={formData.address} onChange={handleInputChange} />
               </div>
 
               <div className="input-body">
                 <label htmlFor="">Location:</label>
-                <input type="text"  />
+                <input type="text" name='location' value={form.location} onChange={handleInputChange} />
               </div>
 
               <div className="input-body">
                 <label htmlFor="">Price:</label>
-                <input type="text"  />
+                <input type="text" name='price' value={formData.price} onChange={handleInputChange} />
               </div>
 
               <div className="imgcard">
