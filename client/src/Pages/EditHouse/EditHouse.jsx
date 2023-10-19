73f2494191d39ca583c665d1f238b9c117b4ebbe
Success-akinyemi <brownsamuel915@gmail.com>
2023-10-19 01:28:48 -0700

    Vercel config

--- client/src/Pages/EditHouse/EditHouse.jsx
+++ client/src/Pages/EditHouse/EditHouse.jsx
@@ -4,6 +4,8 @@ import Navbar from '../../Components/Navbar/Navbar'
 import DropDown from '../../Components/DropDown/DropDown'
 import Footer from '../../Components/Footer/Footer'
 import { useFetchHouses } from '../../hooks/fetch.hooks'
+import { useState } from 'react'
+import Spinner from '../../Components/Helpers/Spinner/Spinner'
 
 function EditHouse({isOpen, toggle}) {
     const loc = useLocation()
@@ -20,6 +22,7 @@ function EditHouse({isOpen, toggle}) {
       location: location || '',
       price: price || '',
     });
+    
   
     const handleInputChange = (e) => {
       const { name, value } = e.target;
@@ -39,7 +42,7 @@ function EditHouse({isOpen, toggle}) {
         }
       }
       // Send updatedFields to your API for updating the house
-      console.log(updatedFields);
+      console.log('updatedFields>>',updatedFields);
     };
 
   return (
@@ -47,55 +50,61 @@ function EditHouse({isOpen, toggle}) {
         <Navbar toggle={toggle} />
         <DropDown isOpen={isOpen} toggle={toggle} />
         <div className="editHouse-container">
-            <h2>EditHouse</h2>
-            <form onSubmit={handleFormSubmit}>
-              <div className="input-body">
-                <label htmlFor="">Tilte:</label>
-                <input type="text" name='title' value={formData.title} onChange={handleInputChange} />
-              </div>
-
-              <div className="input-body">
-                <label htmlFor="">Description:</label>
-                <input type="text" name='desc' value={formData.desc} onChange={handleInputChange} />
-              </div>
-
-              <div className="input-body">
-                <label htmlFor="">Address:</label>
-                <input type="text" name='address' value={formData.address} onChange={handleInputChange} />
-              </div>
-
-              <div className="input-body">
-                <label htmlFor="">Location:</label>
-                <input type="text" name='location' value={form.location} onChange={handleInputChange} />
-              </div>
-
-              <div className="input-body">
-                <label htmlFor="">Price:</label>
-                <input type="text" name='price' value={formData.price} onChange={handleInputChange} />
-              </div>
-
-              <div className="imgcard">
-                <label htmlFor="">Current House Image:</label>
-                <img src={image} alt='house' />
-              </div>
-
-              <div className="imgArrayCard">
-                <label htmlFor="">Current Display Images:</label>
-                <div className="images">
-                  {Array.isArray(imageArray) ? (
-                    imageArray.map((item, idx) => (
-                    <img src={item} alt='house-image' key={idx}/>
-                    ))
-                  ) : (
-                  <p>No images available</p>
-                  )}
+            <h2>EditHouse </h2>
+            {
+              isLoadingHouseData ? (
+                <Spinner />
+              ) : (
+                <form onSubmit={handleFormSubmit}>
+                <div className="input-body">
+                  <label htmlFor="">Tilte:</label>
+                  <input type="text" name='title' value={formData.title} onChange={handleInputChange} />
                 </div>
-              </div>
-
-              <div className="btn">
-                <button>Update</button>
-              </div>
-            </form>
+  
+                <div className="input-body">
+                  <label htmlFor="">Description:</label>
+                  <input type="text" name='desc' value={formData.desc} onChange={handleInputChange} />
+                </div>
+  
+                <div className="input-body">
+                  <label htmlFor="">Address:</label>
+                  <input type="text" name='address' value={formData.address} onChange={handleInputChange} />
+                </div>
+  
+                <div className="input-body">
+                  <label htmlFor="">Location:</label>
+                  <input type="text" name='location' value={formData.location} onChange={handleInputChange} />
+                </div>
+  
+                <div className="input-body">
+                  <label htmlFor="">Price:</label>
+                  <input type="text" name='price' value={formData.price} onChange={handleInputChange} />
+                </div>
+  
+                <div className="imgcard">
+                  <label htmlFor="">Current House Image:</label>
+                  <img src={image} alt='house' />
+                </div>
+  
+                <div className="imgArrayCard">
+                  <label htmlFor="">Current Display Images:</label>
+                  <div className="images">
+                    {Array.isArray(imageArray) ? (
+                      imageArray.map((item, idx) => (
+                      <img src={item} alt='house-image' key={idx}/>
+                      ))
+                    ) : (
+                    <p>No images available</p>
+                    )}
+                  </div>
+                </div>
+  
+                <div className="btn">
+                  <button>Update</button>
+                </div>
+              </form>
+              )
+            }
         </div>
         <Footer />
     </div>
